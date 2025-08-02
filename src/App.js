dd25b422d87282b400d58f0570da177894111cbb
Moses-beniyel <mosesbeniyel007@gmail.com>
2025-08-02 18:36:15 +0530

    dlete the about component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import Intro from "./components/Intro";
-import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
